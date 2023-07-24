09650175a1ea2021d43fc860a172930cb5d826db
Buzurgmehr01 <majidovbuzurgmehr70@gmail.com>
2023-07-24 14:45:40 +0500

    Calendar

--- Calendar/Controllers/CalendarVC.swift
+++ Calendar/Controllers/CalendarVC.swift
@@ -9,22 +9,39 @@ import UIKit
 import SnapKit
 
 class CalendarVC: UIViewController {
-
+    
+    let weekArray = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"]
+    
     let viewCalendar = CalendarView()
+    let calendarView = UICalendarView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-  
+        
         configureCalendar()
+        configureWeekStackView()
+    }
 
+    func configureWeekStackView(){
+        
+        for i in weekArray{
+            let req = WeekView()
+            req.weekLabel.text = i
+            if (i == "СБ") || (i == "ВС") {
+                req.weekLabel.textColor = .red
+            }
+            req.snp.makeConstraints { make in
+                make.width.equalTo(40)
+                make.height.equalTo(20)
+            }
+            viewCalendar.weekStackView.addArrangedSubview(req)
+        }
     }
     
     func configureCalendar(){
         view.backgroundColor = .systemGray6
         view.addSubview(viewCalendar)
-        
-        
-        let calendarView = UICalendarView()
+ 
         let calendar = Calendar.current
         let currentDate = Date()
         let startDate = calendar.date(byAdding: .month, value: -12, to: currentDate) ?? calendar.startOfDay(for: .distantPast)
@@ -35,6 +52,10 @@ class CalendarVC: UIViewController {
         calendarView.layer.cornerRadius = 15
         calendarView.backgroundColor = .white
         calendarView.fontDesign = .rounded
+        calendarView.delegate = self
+        
+        let dateSelection = UICalendarSelectionMultiDate(delegate: self)
+        calendarView.selectionBehavior = dateSelection
         
         view.addSubview(calendarView)
         calendarView.snp.makeConstraints {
@@ -45,5 +66,17 @@ class CalendarVC: UIViewController {
         viewCalendar.snp.makeConstraints {
             $0.top.left.right.equalToSuperview()
         }
+    }
+}
+
+
+extension CalendarVC: UICalendarViewDelegate, UICalendarSelectionMultiDateDelegate{
+    
+    func multiDateSelection(_ selection: UICalendarSelectionMultiDate, didSelectDate dateComponents: DateComponents) {
+       
+    }
+
+    func multiDateSelection(_ selection: UICalendarSelectionMultiDate, didDeselectDate dateComponents: DateComponents) {
+     
     }
 }
--- Calendar/View/CalendarView.swift
+++ Calendar/View/CalendarView.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import SnapKit
 
 class CalendarView: UIView {
     
     let titleLabel = UILabel()
-    
+    let resetButton = UIButton()
+    var weekStackView = UIStackView()
+        
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -22,15 +25,57 @@ class CalendarView: UIView {
     
     func configure(){
         
-        self.backgroundColor = .systemGray6
+        self.backgroundColor = .blue
         self.addSubview(titleLabel)
+        self.addSubview(resetButton)
+        self.addSubview(weekStackView)
+        
         
         
         titleLabel.text = "Когда"
         titleLabel.textAlignment = .center
         titleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(10)
+            $0.top.equalToSuperview().offset(15)
             $0.left.right.equalToSuperview().inset(26)
         }
+        
+        resetButton.setTitle("Сбросить", for: .normal)
+        resetButton.setTitleColor(.systemBlue, for: .normal)
+        resetButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.right.equalToSuperview().inset(26)
+        }
+        
+        weekStackView.axis = .horizontal
+        weekStackView.spacing = 6
+        weekStackView.distribution = .fillProportionally
+        weekStackView.layoutMargins = UIEdgeInsets(top: 0, left: 26, bottom: 0, right: 26)
+        weekStackView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(10)
+            $0.left.right.equalToSuperview().offset(26)
+        }
+    }
+}
+
+
+class WeekView: UIView{
+    
+    let weekLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.addSubview(weekLabel)
+        
+        weekLabel.font = UIFont(name: "Avenir-Light", size: 10)
+        
+        weekLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(5)
+            $0.left.equalToSuperview().inset(10)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
